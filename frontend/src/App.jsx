48a24f35f0afffa6b49f31ffc14024c6d2cb39b0
Jahangir Alom <alomj257@gmail.com>
2025-03-07 16:09:42 +0530

    tailwind configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-2xl font-bold text-center text-blue-600'>Tailwind cheeck</p>
+    </div>
+  )
+}
+
+export default App
